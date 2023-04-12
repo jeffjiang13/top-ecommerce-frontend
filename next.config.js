321f9f90b8837b7ffad5c5b318f2699cf61b3cc1
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-12 19:12:22 -0400

    first commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-  // Remove the experimental property as a test
-  images: {
-    domains: ["i.ibb.co", "ibb.co"],
-  },
   env: {
     publishableKey: `${process.env.STRIPE_PUBLIC_KEY}`,
   },
@@ -14,5 +10,8 @@ module.exports = withPWA({
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
+  },
+  images: {
+    domains: ["i.ibb.co", "ibb.co"],
   },
 });
