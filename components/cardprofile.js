b76e6530a39890a870f6d44b212f610afe83c36b
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-29 23:16:27 -0400

    fiex profile image

--- components/cardprofile.js
+++ components/cardprofile.js
@@ -7,7 +7,9 @@ function CardProfile({ session: initialSession, orders }) {
   const [session, setSession] = useState(initialSession);
   const [imageUrl, setImageUrl] = useState(
     initialSession.profileImage
-      ? `${process.env.NEXT_PUBLIC_APIURL}${initialSession.profileImage.url}`
+      ? initialSession.profileImage.provider === 'cloudinary'
+        ? initialSession.profileImage.url
+        : `${process.env.NEXT_PUBLIC_APIURL}${initialSession.profileImage.url}`
       : ""
   );
   const [isLoading, setIsLoading] = useState(true);
@@ -32,7 +34,9 @@ function CardProfile({ session: initialSession, orders }) {
         setSession(response.data);
         if (response.data.profileImage) {
           setImageUrl(
-            `${process.env.NEXT_PUBLIC_APIURL}${response.data.profileImage.url}`
+            response.data.profileImage.provider === 'cloudinary'
+              ? response.data.profileImage.url
+              : `${process.env.NEXT_PUBLIC_APIURL}${response.data.profileImage.url}`
           );
         }
       } catch (error) {
@@ -73,7 +77,11 @@ function CardProfile({ session: initialSession, orders }) {
         ...session,
         profileImage: res.data[0],
       });
-      setImageUrl(`${process.env.NEXT_PUBLIC_APIURL}${res.data[0].url}`);
+      setImageUrl(
+        res.data[0].provider === 'cloudinary'
+          ? res.data[0].url
+          : `${process.env.NEXT_PUBLIC_APIURL}${res.data[0].url}`
+      );
     } catch (err) {
       console.log("Image upload error:", err);
     }
@@ -99,72 +107,72 @@ function CardProfile({ session: initialSession, orders }) {
               alt="Profile"
               onLoad={() => setIsLoading(false)}
             />
-          ) : (
-            <div className="w-24 h-24 rounded-full bg-black mb-2"></div>
-          )}
-          <input
-            type="file"
-            id="upload-image"
-            className="hidden"
-            accept="image/*"
-            onChange={onImageChange}
-          />
-        </label>
-        <div className="text-center">
-          <p className="mb-1">{session.username}</p>
-          <p className="text-xs text-gray-400 mb-1">Verified Account</p>
-          <p className="text-xs text-gray-400">{session.email}</p>
-        </div>
-      </div>
-      <div className="py-3 flex flex-col place-items-center">
-        <div className="flex place-items-center mb-2 font-semibold">
-          <svg
-            className="w-4 h-4 text-black mr-1"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
+            ) : (
+              <div className="w-24 h-24 rounded-full bg-black mb-2"></div>
+            )}
+            <input
+              type="file"
+              id="upload-image"
+              className="hidden"
+              accept="image/*"
+              onChange={onImageChange}
             />
-          </svg>
-          <p className="text-xs text-black">Member+</p>
+          </label>
+          <div className="text-center">
+            <p className="mb-1">{session.username}</p>
+            <p className="text-xs text-gray-400 mb-1">Verified Account</p>
+            <p className="text-xs text-gray-400">{session.email}</p>
+          </div>
         </div>
-        <div className="text-center text-xs mb-2">
-          <p className="text-black mb-1">Payment succeeded :</p>
-          <p className="text-gray-400">{orders.length} times</p>
+        <div className="py-3 flex flex-col place-items-center">
+          <div className="flex place-items-center mb-2 font-semibold">
+            <svg
+              className="w-4 h-4 text-black mr-1"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
+              />
+            </svg>
+            <p className="text-xs text-black">Member+</p>
+          </div>
+          <div className="text-center text-xs mb-2">
+            <p className="text-black mb-1">Payment succeeded :</p>
+            <p className="text-gray-400">{orders.length} times</p>
+          </div>
+          <div className="text-center text-xs">
+            <p className="text-black mb-1">Money Spent :</p>
+            <NumberFormat
+              value={orders.reduce(
+                (val, order) =>
+                  val +
+                  order.items.reduce((v, i) => v + i.amount_subtotal / 100, 0),
+                0
+              )}
+              className="text-gray-400 text-xs"
+              displayType={"text"}
+              thousandSeparator={true}
+              prefix={"$"}
+              renderText={(value, props) => (
+                <p className="text-gray-400 text-xs" {...props}>
+                  {value}
+                </p>
+              )}
+            />
+          </div>
         </div>
-        <div className="text-center text-xs">
-          <p className="text-black mb-1">Money Spent :</p>
-          <NumberFormat
-            value={orders.reduce(
-              (val, order) =>
-                val +
-                order.items.reduce((v, i) => v + i.amount_subtotal / 100, 0),
-              0
-            )}
-            className="text-gray-400 text-xs"
-            displayType={"text"}
-            thousandSeparator={true}
-            prefix={"$"}
-            renderText={(value, props) => (
-              <p className="text-gray-400 text-xs" {...props}>
-                {value}
-              </p>
-            )}
-          />
-        </div>
+        <p className="text-gray-400 text-xs">
+          Shop now and earn a new pair of shoes with every $1000 spent - loyalty
+          has never looked so stylish!
+        </p>
       </div>
-      <p className="text-gray-400 text-xs">
-        Shop now and earn a new pair of shoes with every $1000 spent - loyalty
-        has never looked so stylish!
-      </p>
-    </div>
-  );
-}
+    );
+  }
 
-export default CardProfile;
+  export default CardProfile;
